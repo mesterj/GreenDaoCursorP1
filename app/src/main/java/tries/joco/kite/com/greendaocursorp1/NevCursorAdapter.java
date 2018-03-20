c81c06bf42d14a57808c95001a181d16d993133c
DESKTOP-MFIIQC5\meste <mester@kite.hu>
2018-03-20 14:45:55 +0100

    fasz tudja mi ez...

---
+++ app/src/main/java/tries/joco/kite/com/greendaocursorp1/NevCursorAdapter.java
@@ -0,0 +1,27 @@
+package tries.joco.kite.com.greendaocursorp1;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v4.widget.CursorAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by meste on 2018. 03. 20..
+ */
+
+public class NevCursorAdapter extends CursorAdapter {
+    public NevCursorAdapter(Context context, Cursor c, boolean autoRequery) {
+        super(context, c, autoRequery);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+    }
+}
